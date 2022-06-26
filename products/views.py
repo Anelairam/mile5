88f6d8d2e0356604923c8a376a67848703e65c2c
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2022-06-26 16:58:55 +0000

    aADd product details functionality

--- products/views.py
+++ products/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 # Create your views here.
 
 def products(request):
-    """A view returning the index page"""
+    """A view returning the product page"""
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
     
-    return render(request, 'products/products.html')
+    return render(request, 'products/products.html', context)
+
+def product_detail(request, product_id):
+    """A view returning the product detail page"""
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
